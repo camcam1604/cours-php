0556a5a75fc119081e0917f6aef9bda7fbe75300
camcam1604 <camille.parisot@my-digital-school.org>
2024-06-03 15:42:57 +0200

    push

--- 07_array_function.php
+++ 07_array_function.php
@@ -36,6 +36,7 @@ echo "<br>";
 
 $chunkedArray= array_chunk($fruits,2);
 var_dump($chunkedArray);
+
 echo "<br>";
 
 $arr1 = [1, 2, 3];
@@ -45,4 +46,39 @@ var_dump($arr3);
 
 $arr4 = [...$arr1,...$arr2];
 var_dump($arr4);
+
+$colors = ['green', 'red','yellow'];
+$fruits = ['avocado', 'apple', 'banana'];
+$result = array_combine($colors, $fruits);
+
+echo $result['green'];
+
+echo "<br>"; 
+
+$keys = array_keys($result);
+var_dump($keys);
+print_r($keys);
+
+echo "<br>";
+
+$flipped = array_flip($result);
+var_dump($flipped);
+
+echo "<br>";
+
+$number = range(1, 20);
+var_dump($number);
+
+$newNumbers = array_map(function($number){
+    return "Number {$number}";
+}, $number);
+var_dump($newNumbers);
+
+$lessThan10 = array_filter($number, fn($number) => $number < 10);
+var_dump($lessThan10);
+$moreThan10 = array_filter($number, fn($number) => $number > 10);
+var_dump($moreThan10);
+
+$sum = array_reduce($number, fn($carry, $number) => $carry + $number);
+var_dump($sum);
 ?>
